61d65162ce85faebbcfb82f26fd8520509def291
Jackson Mazza <jaxtomax2@gmail.com>
2025-09-28 18:57:21 -0700

    Created the GlobeRect class object

--- main.py
+++ main.py
@@ -7,6 +7,17 @@ sys.setrecursionlimit(10**6)
 
 calpoly_email_addresses = ["jjmazza@calpoly.edu"]
 
+#Creates a class for rectangles on a globe
+@dataclass(frozen=True)
+class GlobeRect:
+    #latitudes must be from -90 to 90
+    lo_lat = int #must be lower than hi_lat
+    hi_lat = int
+    #longitudes must be from -180 to 180
+    #if west_long == east_long, then width = 0
+    west_long = int
+    east_long = int
+
 # put all test cases in the "Tests" class.
 class Tests(unittest.TestCase):
     def test_example_1(self):
