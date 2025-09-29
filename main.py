f4cabd4eb7e8a7e4d195b09bd81c565451b3345c
Jackson Mazza <jaxtomax2@gmail.com>
2025-09-28 19:44:05 -0700

    added comments for class definitions

--- main.py
+++ main.py
@@ -10,28 +10,33 @@ calpoly_email_addresses = ["jjmazza@calpoly.edu"]
 #Creates a class for rectangles on a globe
 @dataclass(frozen=True)
 class GlobeRect:
-    #latitudes must be from -90 to 90
-    lo_lat = int #must be lower than hi_lat
-    hi_lat = int
-    #longitudes must be from -180 to 180
+    #latitudes must be from -90.0 to 90.0
+    lo_lat = float #must be lower than hi_lat
+    hi_lat = float
+    
+    #longitudes must be from -180.0 to 180.0
     #if west_long == east_long, then width = 0
-    west_long = int
-    east_long = int
+    west_long = float
+    east_long = float
 
 #Creates a class that takes a rectangle from GlobeRect and describes it
 @dataclass(frozen=True)
 class Region:
-    area : GlobeRect
-    name : str
-    terrain : str
+    rect : GlobeRect #the area of the specific GlobeRect
+    name : str #The name of the region
+    terrain : str #being the type of terrain/biome (ie forest, mountain, etc)
 
 #Creates a class that holds the details of a specific region
 @dataclass(frozen=True)
 class RegionCondition:
     region : Region
-    year : int
-    pop : int
-    ghg_rate : float
+    year : int #The year of the region
+    pop : int #The population of people in the region
+    ghg_rate : float #in tons of C02 equivalent per year
+
+#This function describes the region condition of Seattle, WA
+def Seattle(reg:RegionCondition):
+
 
 # put all test cases in the "Tests" class.
 class Tests(unittest.TestCase):
