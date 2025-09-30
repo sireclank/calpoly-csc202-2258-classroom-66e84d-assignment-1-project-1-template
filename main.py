3ecf8e4d79637048c2cd4c2265ba92ddf9168a89
Jackson Mazza <jaxtomax2@gmail.com>
2025-09-30 13:22:41 -0700

    Need to seek help with A1 and finishing it. Will try to schedule office hours

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from typing import *
-from dataclasses import dataclass
+import sys
 import unittest
 import math
-import sys
+from typing import *
+from dataclasses import dataclass
 sys.setrecursionlimit(10**6)
 
 calpoly_email_addresses = ["jjmazza@calpoly.edu"]
@@ -30,37 +30,42 @@ class RegionCondition:
     pop : int #The population of people in the region
     ghg_rate : float 
 
-region1 : RegionCondition = RegionCondition(Region(GlobeRect(47.49616, 47.73464, -122.43689, -122.23591), "Seattle", "Port City"), 2022, 751454, 2980000.0)
-region2 : RegionCondition = RegionCondition(Region(GlobeRect(44.47121, 44.57550, -88.14111, -87.84214), "Green Bay", "Port City"), 2018, 104756, 2780000.0)
+region1 : RegionCondition = RegionCondition(Region(GlobeRect(47.49616, 47.73464, -122.43689, -122.23591), "Seattle", "Other"), 2022, 751454, 2980000.0)
+region2 : RegionCondition = RegionCondition(Region(GlobeRect(44.47121, 44.57550, -88.14111, -87.84214), "Green Bay", "Other"), 2018, 104756, 2780000.0)
 region3 : RegionCondition = RegionCondition(Region(GlobeRect(34.62524, 52.01460, -155.43856, -132.83251), "North Pacific", "Ocean"), 2020, 0, 0.0)
-region4 : RegionCondition = RegionCondition(Region(GlobeRect(35.23421, 35.31154, -120.72305, -120.61785), "SLO", "Agriculture City"), 2021, 47545, 151000.0)
+region4 : RegionCondition = RegionCondition(Region(GlobeRect(35.23421, 35.31154, -120.72305, -120.61785), "SLO", "Other"), 2021, 47545, 151000.0)
 
 example_region_conditions : List[RegionCondition] = [region1, region2, region3, region4]
 
 #This function calculates the tons of CO2 emitted by a single person per year
-def emissions_per_capita(r:RegionCondition) -> float:
+def emissions_per_capita(r : RegionCondition) -> float:
     if r.pop <= 0:
         raise ValueError("Population can not be 0")
     return r.ghg_rate / r.pop
 
 #This function calculates the area (in square kilometers) of GlobeRect
-def area(gr:GlobeRect) -> float:
+def area(gr : GlobeRect) -> float:
     r : float = 6371.0
     init_area : float = 2 * math.pi * r * ((r * math.sin(gr.hi_lat)) - (r * math.sin(gr.lo_lat)))
     actual_area : float = init_area / (gr.west_long - gr.east_long)
     return actual_area
 
 #This function calculates the CO2 emissions per km^2
-def emissions_per_square_km(c:RegionCondition) -> float:
+def emissions_per_square_km(c : RegionCondition) -> float:
     if area(c.region.rect) <= 0:
         raise ValueError("Area can not be 0")
     return c.ghg_rate / area(c.region.rect)
 
+#determines which region has most population density
 def densest(l1 : List[RegionCondition]) -> str:
     max : float = l1[0].pop / area(l1[0].region.rect)
     for i in range(len(l1)):
         if l1[i].pop / area(l1[i].region.rect) > max:
+            max = l1[i].pop / area(l1[i].region.rect)
             return l1[i].region.name
+        
+#returns an estimated region's condition after a set amount of years
+def project_condition(r : RegionCondition, x : int) -> RegionCondition:
 
 
 # put all test cases in the "Tests" class.
@@ -76,6 +81,9 @@ class Tests(unittest.TestCase):
 
     def test_densest(self):
         self.assertEqual(densest(example_region_conditions), "Seattle")
+
+    def test_project_condition(self):
+        self.assertEqual(project_condition(region1, 2), RegionCondition)
 
 if (__name__ == '__main__'):
     unittest.main()
